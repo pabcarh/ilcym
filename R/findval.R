efff1aaca1692204ec4adf05075a69defdecba8f
pabcarh <pabcarh@hotmail.com>
2016-11-14 16:32:44 -0500

    second update

---
+++ R/findval.R
@@ -0,0 +1,25 @@
+#' temp
+#'
+#' development time sub function
+#'
+#' no details
+#'
+#' @param dat data frame.
+#' @param cate vector of characters.
+#' @return character a string; default is 'finished!'.
+#' @author Pablo Carhuapoma Ramos
+#' @family example
+#' @example inst/examples/example_findval.R
+#' @export
+findval<-function(dat,cate)
+{n1=ncol(dat)
+result=rep(0,n1)
+for(i in 1:n1)
+{
+  p1=unique(dat[,i])==cate
+  if(length(p1[p1==TRUE])>=1){result[i]=1}else{result[i]=0}
+}
+ntot=sum(result)
+if(ntot>=1){exist=TRUE}else{exist=FALSE}
+return(list(ntot=ntot,exist=exist))
+}
